c62a203a84a84dee2becd0a7cc2f304d5cbe4874
Grant Dong <48699101+donggrant@users.noreply.github.com>
2022-04-23 17:19:43 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,28 +9,22 @@ from PIL import Image
 import time
 import threading
 
-
-
-"""
-Demo program that displays a webcam using OpenCV
-"""
-
-def run_model(model, class_names, frame, window, target):
-    cv2.imwrite("opencv_testpic.png", frame)
-    img = Image.open("opencv_testpic.png")
+def run_model(model, class_names, frame, window, target, img_name):
+    cv2.imwrite(img_name, frame)
+    img = Image.open(img_name)
     img = np.asarray(img)
     img = cv2.resize(img,(128,128))
     img = img.reshape(1, 128, 128,3)
     output = model.predict(img)[0]
     sign = class_names[np.argmax(output)]
-    if output[target] > 0.5:
+    if len(output) == 24 and output[target] > 0.5:
         window['-OUTPUT-'].update(class_names[target])
     else:
         window['-OUTPUT-'].update(sign)
 
 def main():
 
-    phrase = "HELLOWORLD"
+    phrase = "BEWARE"
     index = 0
 
 
@@ -40,14 +34,16 @@ def main():
     sg.theme('Black')
 
     class_names = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y' ]
+    vowel_classes = ['A', 'E', 'I', 'O', 'U']
 
     cnn_model = tf.keras.models.load_model("cnn_model.h5")
     tf_model = tf.keras.models.load_model("TF_model.h5")
+    vowel_model = tf.keras.models.load_model("cnn_model_vowel.h5")
 
     # define the window layout
-    layout = [[sg.Text('Sign Language Demo', size=(40, 1), justification='center', font='Helvetica 20', key='prompt')],
+    layout = [[sg.Text('Sign Language Demo', size=(30, 1), justification='center', font='Helvetica 20', key='prompt')],
             [sg.Text("Try signing: " + phrase, size=(50,1), font='Helvetica 12', key='-PROMPT-')],
-              [sg.Image(filename='', size=(40, 40), key='image')],
+            [sg.Column([[sg.Image(filename='', size=(10, 50), key='image')]], justification='center')],
               [sg.Button('Start', size=(10, 1), font='Helvetica 10'),
                sg.Button('Exit', size=(10, 1), font='Helvetica 10'), 
                sg.Text(size=(3,1), font='Helvetica 15', key='-OUTPUT-'),
@@ -67,7 +63,7 @@ def main():
 
         event, values = window.read(timeout=20)
         if event == 'Exit' or event == sg.WIN_CLOSED:
-            return
+            break
 
         elif event == 'Start':
             recording = True
@@ -79,9 +75,12 @@ def main():
             seconds = time.time()
             if seconds - prev > 0.5:
                 prev = seconds
-                threading.Thread(target=run_model, args=(cnn_model, class_names, frame, window, class_names.index(phrase[index]))).start()
-                threading.Thread(target=run_model, args=(tf_model, class_names, frame, window, class_names.index(phrase[index]))).start()
-
+                threading.Thread(target=run_model, args=(cnn_model, class_names, frame, window, 
+                class_names.index(phrase[index]), "pic1.png")).start()
+                threading.Thread(target=run_model, args=(tf_model, class_names, frame, window, 
+                class_names.index(phrase[index]), "pic2.png")).start()
+                threading.Thread(target=run_model, args=(vowel_model, vowel_classes, frame, window, 
+                class_names.index(phrase[index]), "pic3.png")).start()
 
             imgbytes = cv2.imencode('.png', frame)[1].tobytes()  # ditto
             window['image'].update(data=imgbytes)
@@ -92,5 +91,6 @@ def main():
                 if index == len(phrase):
                     recording = False
                     window['-PROMPT-'].update("Congratulations, you completely signed: " + phrase)
+                time.sleep(0.5)
              
 main()
